1eb4c5a64f77070f0ccfca17cbda910bd1d7134b
Waldemar Głaz <w.glaz@cheil.com>
2023-01-02 10:13:07 +0100

    Header

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <main>
+      <Header /> form
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <h1>
+      SPLI
+      <br />
+      TTER
+    </h1>
+  );
+};
+
+export default Header;
